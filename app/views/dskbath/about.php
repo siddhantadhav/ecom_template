8a84db0e1e7dbb8516ea0a4614149eb4365a0926
kathoreakshata <digital.cmq@gmail.com>
2023-05-15 18:15:08 +0530

    alt text added to about us page

--- app/views/dskbath/about.php
+++ app/views/dskbath/about.php
@@ -9,31 +9,42 @@
 
     #counter {
         color: rgb(36, 188, 189);
-        font-size: 5rem;
+        font-size: 3rem;
     }
 
     #counter_cleint {
         color: rgb(36, 188, 189);
-        font-size: 5rem;
+        font-size: 3rem;
+    }
+    .btn:hover{
+        background-color: white;
+        border-color:white;
+        box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;   
+    }
+    @media only screen and (max-width: 768px) {
+        .query-txt, .query-btn{
+            display: grid;
+            justify-content: center;
+        }
     }
 </style>
 <div>
     <img src="<?= ASSETS . THEME ?>images/about/about_us_banner.jpg"
-         alt=""
+         alt="about_us_banner"
          class="img-fluid">
 </div>
 
 <section style="margin-top: 0;">
     <div class="container-fluid">
         <div class="row align-items-center">
-            <div class="col-lg-6 col-md-12">
-                <h1 class="display-2"
+        <h1 class="display-2 text-center pt-2"
                     style="color: rgb(36, 188, 189);" data-aos="fade-up"
      data-aos-duration="1000">About Us</h1>
-                <h2 class="display-5"
+            <div class="col-lg-6 col-md-12">                
+                <h2 class="display-3"
                     style="color: rgb(110, 110, 110);" data-aos="fade-up"
      data-aos-duration="1000">Introduction To Our Core Purpose</h2>
-                <p class="display-5"
+                <p class="display-4"
                    style="color: rgb(36, 188, 189);" data-aos="fade-up"
      data-aos-duration="1000">Enthusiastically endeavor and enhance customer's quality of life.
                 </p>
@@ -45,7 +56,7 @@
             <div class="col" data-aos="fade-left"
      data-aos-duration="1000">
 
-                <div class="row mt-5">
+                <div class="row mt-3">
                     <div class="col-1 gx-0 d-none d-md-block d-lg-block d-xl-block"
                          style="background-color: #eee; margin-bottom: 10vh;"></div>
                     <div class="col gx-0"
@@ -53,7 +64,7 @@
                         <div style="background-color: #eee; height: 10vh; "></div>
                         <div>
                             <img src="<?= ASSETS . THEME ?>images/about/about_us_section_1.jpg"
-                                 alt=""
+                                 alt="about_us_intro_image"
                                  class="img-fluid"
                                  style="box-shadow: rgba(0, 0, 0, 0.4) 0px 30px 90px;">
                         </div>
@@ -76,7 +87,7 @@
                         <div style="background-color: #eee; height: 10vh; "></div>
                         <div>
                             <img src="<?= ASSETS . THEME ?>images/about/about_us_section_2.jpeg"
-                                 alt=""
+                                 alt="about_us_section_2.jpeg"
                                  class="img-fluid"
                                  style="box-shadow: rgba(0, 0, 0, 0.4) 0px 30px 90px; width: 100%;">
                         </div>
@@ -90,10 +101,10 @@
             </div>
 
             <div class="col mt-5">
-                <span class="display-5"
+                <span class="display-4 px-1"
                       style="background-color: rgb(36, 188, 189); color: white;" data-aos="fade-up"
      data-aos-duration="1000">True facts </span>
-                <h2 class="display-4" data-aos="fade-up"
+                <h2 class="display-3 mt-2" data-aos="fade-up"
      data-aos-duration="1000">Whats makes us different?</h2>
                 <p class="text-muted" data-aos="fade-up"
      data-aos-duration="1000">DSK has created a niche for itself in the segment of bathroom fittings, making it
@@ -114,12 +125,12 @@
                     <div class="col-lg-6 col-md-6 col-sm-12" data-aos="zoom-in"
      data-aos-duration="1000">
                         <p id="counter" >0</p>
-                        <span style="font-size: 2.5rem; color: rgb(36, 188, 189);"> + Products</span>
+                        <span style="font-size: 2.1rem; color: rgb(36, 188, 189);"> + Products</span>
                     </div>
                     <div class="col" data-aos="zoom-in"
      data-aos-duration="1000">
                         <p id="counter_cleint">0</p>
-                        <span style="font-size: 2.5rem; color: rgb(36, 188, 189)"> + Client Works</span>
+                        <span style="font-size: 2.1rem; color: rgb(36, 188, 189)"> + Client Works</span>
                     </div>
                 </div>
             </div>
@@ -128,14 +139,14 @@
 </section>
 
 <section style="background-color: rgb(36, 188, 189)">
-    <div class="row align-items-center justifty-content-center"
+    <div class="row"
          style="padding: 2vh">
-        <div class="col-7 ">
-            <h4 class="text-end"
+        <div class="col-lg-8 col-md-12 col-sm-12">
+            <h4 class="text-end query-txt"
                 style="color: white">For any further details or inquiries, Please contact Here!</h4>
         </div>
-        <div class="col">
-            <a href="<?= ROOT ?>contact">
+        <div class="col-lg-4 col-md-12 col-sm-12">
+            <a href="<?= ROOT ?>contact" class="query-btn">
                 <button type="button"
                         class="btn btn-light"
                         style="color: rgb(36, 188, 189)">Contact Us</button></a>
